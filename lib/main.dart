e6b54c390ffa6bee7b547c67a2493d8183d53004
Maduoma <18477889+maduoma@users.noreply.github.com>
2022-01-04 20:23:20 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+//App's entry point
+void main() => runApp(const ContactProfilePage());
+
+class ContactProfilePage extends StatelessWidget {
+  const ContactProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          //Adding background color to AppBar
+          backgroundColor: Colors.white,
+
+          //Adding back arrow for leading property.
+          //Back arrow appears at the top-left of AppBar
+          leading: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          actions: <Widget>[
+            //Adding Star icon for favorting the contact.
+            //This IconButton responds to user's tap,
+            //and prints the message.
+            IconButton(
+              icon: const Icon(Icons.star_border),
+              color: Colors.black,
+              onPressed: () {
+                print("Contact is starred");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
